827875395f80622fc33e9c39865651b14b491ea8
LorrayneCarvalho <lorraynecc.1106@gmail.com>
2022-03-25 15:38:36 -0300

    Inserting data into the application

--- js/script.js
+++ js/script.js
@@ -4,16 +4,15 @@ let button = document.querySelector('#botao'); //pegando o elemento pelo id
 
 let list = document.querySelector('#list'); //pegando o elemento pelo id
 
-let tasks = [
-    'Work',
-    'Estudar Js', 
-    'Emagrecer',
-];
+let card = document.querySelector('.card');
+
+let tasks = [];
 
 function renderTasks(){
+    list.innerHTML = '';
     for(task of tasks){
         //Criar o item da lista
-       let = document.createElement('li');
+       let itemList = document.createElement('li');
 
        //Adicionar classes do item da lista
        itemList.setAttribute('class', 'list-group-item list-group-item-action');
@@ -31,3 +30,31 @@ function renderTasks(){
 }
 
 renderTasks();
+
+//aplicando funcionalidade ao botão 
+button.onclick = function(){
+    let newTask = input.value;
+
+    if(newTask != ''){
+        tasks.push(newTask);
+        renderTasks();
+        input.value = ''
+        removerSpans();
+    }else {
+        removerSpans();
+        let card = document.querySelector('.card');
+        let span = document.createElement('span');
+        span.setAttribute('class', 'alert alert-warning');
+        let msg = document.createTextNode('You need to inform the task!')
+        span.appendChild(msg);
+        card.appendChild(span);
+    }
+}
+
+function removerSpans(){
+    let spans = document.querySelectorAll('span');
+    
+    for(let i = 0; i < spans.length; i++){
+        card.removeChild(spans[i]);
+    }
+}
